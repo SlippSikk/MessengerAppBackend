f43380798ebc39a65a55efd6244587cac5078b3f
Adam Baqaie <z5362016@ad.unsw.edu.au>
2023-02-22 09:18:08 +1100

    authLoginV1 stub

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,9 @@
+function authRegisterV1(email, password, nameFirst, nameLast) {
+    return {
+        authUserId: 1,
+    };
+}
+
 function authLoginV1(email, password) {
     return {
         authUserId: 1,
