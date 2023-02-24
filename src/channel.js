ffa99f99bf1fb9175d5913e96bac26fce73c25a3
Xuji Li <z5423791@ad.unsw.edu.au>
2023-02-24 14:54:14 +1100

    copy the return value from git lab

--- src/channel.js
+++ src/channel.js
@@ -1,14 +1,17 @@
 function channelMessagesV1(authUserId, channelId, start) {
-     return {messages: [
-        {
-          messageId: 1,
-          uId: 1,
-          message: 'Hello world',
-          timeSent: 1582426789,
-          start: 0,
-          end: 50,
-        }
-      ]
+     return {
+          
+               messages: [
+                 {
+                   messageId: 1,
+                   uId: 1,
+                   message: 'Hello world',
+                   timeSent: 1582426789,
+                 }
+               ],
+               start: 0,
+               end: 50,
+             
 }      
 }
 
