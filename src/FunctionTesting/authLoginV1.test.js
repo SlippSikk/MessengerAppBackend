9539d697d3905919ef8b6c50fcf05cb29f196f08
Adam Baqaie <z5362016@ad.unsw.edu.au>
2023-03-04 16:16:53 +1100

    add test to authLoginV1

--- src/FunctionTesting/authLoginV1.test.js
+++ src/FunctionTesting/authLoginV1.test.js
@@ -1,17 +1,16 @@
 import { authRegisterV1, authLoginV1 } from './../auth.js'
+import { clearV1 } from './../'
 
 // Registration:
-
 beforeEach(() => {
-    clearV1().then(() => {
-        authRegisterV1('adam.baqaie@gmail.com', 'adam123', 'Adam', 'Baqaie');
-    });
+    clearV1();
+    authRegisterV1('adam.baqaie@gmail.com', 'adam123', 'Adam', 'Baqaie');
 });
 
 describe("Correct Login", () => {
     test('Correct Inputs', () => {
         const registered = authLoginV1('adam.baqaie@gmail.com', 'adam123')
-        expect(registered).toEqual({ authUserId: 1 });
+        expect(registered).toEqual({ authUserId: expect.any(Number) });
     });
 });
 
@@ -20,8 +19,13 @@ describe("Incorrect Login", () => {
         { testName: 'incorrect email', email: 'albert.kim@gmail.com', password: 'adam123' },
         { testName: 'incorrect password', email: 'adam.baqaie@gmail.com', password: 'hello1' },
     ])('Input has $testName', ({ email, password }) => {
-        expect(authLoginV1(email, password)).toBe({ error: 'error' });
+        expect(authLoginV1(email, password)).toBe({ error: expect.any(String) });
     });
+
+    test('No Users', () => {
+        clearV1();
+        expect(authLoginV1('adam.b@gmail.com', 'password')).toBe({ error: expect.any(String) });
+    })
 });
 
 
