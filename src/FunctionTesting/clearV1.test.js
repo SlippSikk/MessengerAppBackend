6a3bce6c36a56f2ad8aac8807d0abf1483cb4d9f
Xuji Li <z5423791@ad.unsw.edu.au>
2023-03-05 12:18:31 +1100

    two tests should be added

--- src/FunctionTesting/clearV1.test.js
+++ src/FunctionTesting/clearV1.test.js
@@ -1,31 +1,45 @@
-import { clearV1 } from './other.js'
-import { channelJoinV1 } from '../channel.js'
-import { authRegisterV1 } from '../auth.js'
-import { channelsCreateV1 } from '../channels.js'
-import {getData} from '../dataStore'
+import { clearV1 } from '../other.js';
+
+import { authRegisterV1 } from '../auth.js';
+import { channelsCreateV1 } from '../channels.js';
 
 
-//because it's a black box test, I find that I can test nothing but the return value.
+
+
 
   
   
-  const userA = authRegisterV1('gura@gmail.com', '114514810', 'huaizhi', 'li')
+  const userA = authRegisterV1('gura@gmail.com', '114514810', 'huaizhi', 'li');
   
-  const channelID1 = channelsCreateV1(userA, 'Channel 1', true)
+  const channelID1 = channelsCreateV1(userA, 'Channel 1', true);
  
-  const authID2 = authRegisterV1('Ina@outlook.com', 'asdgf8', 'me', 'vasdui')
+  const authID2 = authRegisterV1('Ina@outlook.com', 'asdgf8', 'me', 'vasdui');
   
-  const channelID2 = channelsCreateV1(authID2, 'Channel 2', true) 
+  const channelID2 = channelsCreateV1(authID2, 'Channel 2', true) ;
 
-  const authID3 = authRegisterV1('lion@qq.com', 'asduif8195', 'agsdfoj', 'antman')
+  const authID3 = authRegisterV1('lion@qq.com', 'asduif8195', 'agsdfoj', 'antman');
 
 
  
-describe("return value of clear", () => {
-  test('Correct Inputs', () => {
+describe("three tests", () => {
+  test('Correct return value', () => {
       
       expect(clearV1()).toStrictEqual({});
   });
+
+/*  two test should be added
+  test('clear the userMember', () => {
+      
+    expect(clearV1()).toStrictEqual({});
+});
+
+
+test('clear the channelDetails', () => {
+      
+  expect(clearV1()).toStrictEqual({});
+});*/
+
+
 });
 
 
