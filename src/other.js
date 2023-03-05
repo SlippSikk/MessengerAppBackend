2f2adb52e84431a66482a1331decf6009f17fbfd
Xuji Li <z5423791@ad.unsw.edu.au>
2023-03-05 12:31:38 +1100

    function clearV1

---
+++ src/other.js
@@ -0,0 +1,14 @@
+/**
+ * Resets the internal data of the application to its initial state
+ * @param none
+ * @returns {{}}
+ */
+export function clearV1(){
+    let data = {
+        userMembers: [],
+        channelDetails: [],
+    }
+    
+      return {};
+    
+}
