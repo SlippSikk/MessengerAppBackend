c8fbde21ed5786e92cb28f8cc1fe6caed72f5e85
John Saul <z5401374@ad.unsw.edu.au>
2023-03-06 21:44:25 +1100

    Fixed import statements

--- src/FunctionTesting/channelInviteV1.test.js
+++ src/FunctionTesting/channelInviteV1.test.js
@@ -1,7 +1,7 @@
-import { channelInviteV1 } from '../channel.js'
-import { authRegisterV1 } from '../auth.js'
-import { channelsCreateV1 } from '../channels.js'
-import { clearV1 } from '../other.js'
+import { channelInviteV1 } from './../channel.js'
+import { authRegisterV1 } from './../auth.js'
+import { channelsCreateV1 } from './../channels.js'
+import { clearV1 } from './../other.js'
 
 /*const authID2 = authRegisterV1('bob@gmail.com', 'efk289', 'Bob', 'Smith')
     const authID3 = authRegisterV1('susan@outlook.com', 'hello321', 'Susan', 'Miller')
