7aee695dc497ce7d391718f0a64f115c25780050
Xuji Li <z5423791@ad.unsw.edu.au>
2023-03-07 18:54:15 +1100

    add document

--- src/users.js
+++ src/users.js
@@ -1,3 +1,13 @@
+
+/**
+ * For a valid user, returns information about their user ID, 
+ * email, first name, last name, and handle
+ *
+ * @param {string} authUserId
+ * @param {string} uID
+ * @returns {{user: object}}
+ */
+
 import { getData } from "./dataStore";
 export function userProfileV1(authUserId, uId){
     let data = getData();
