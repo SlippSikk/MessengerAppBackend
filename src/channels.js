83d4ca63c7bd6c3ebf36cd872410f76875fc2e70
Albert Kim <z5358832@ad.unsw.edu.au>
2023-03-07 18:50:23 +1100

    channelsListV1 is almost in working form

--- src/channels.js
+++ src/channels.js
@@ -1,11 +1,26 @@
+import { getData } from "./dataStore";
+
 function channelsListV1(authUserId) {
-    return {
-        channels: [
-            {
-                channelId: 1,
-                name: 'My Channel',
+    // Error: invalid user ID
+    if (!getData().userMembers.userID.includes(authUserId)) {
+        return { error: 'authUserId is invalid' };
+    }
+
+    let channels = [];
+
+    for (let i = 0; i < getData().channelDetails.length; i++) {
+        if (getData().channelDetails[i].memberIDs.includes(authUserId) == true) {
+            let curr_channel = {
+                channelId: getData().channelDetails[i].channelID,
+                name: getData().channelDetails[i].channelName
             }
-        ],
+
+            channels.push(curr_channel);
+        }
+    }
+
+    return {
+        channels
     };
 }
 
@@ -15,14 +30,14 @@ function channelsCreateV1(authUserId, name, isPublic) {
     };
 }
 
-function channelsListAllV1 (authUserId) {
-     return {
-          channels: [
+function channelsListAllV1(authUserId) {
+    return {
+        channels: [
             {
-              channelId: 1,
-              name: 'My Channel',
+                channelId: 1,
+                name: 'My Channel',
             }
-          ],
-        };
+        ],
+    };
 
 }
