2d2448845b68c64f2d5734b1547676284e73ce33
John Saul <z5401374@ad.unsw.edu.au>
2023-03-08 10:55:56 +1100

    exported function

--- src/helper.js
+++ src/helper.js
@@ -6,7 +6,7 @@
  */
 // check if an object exists in an array of objects based on searchID
 // returns the index of that object if it exists, otherwise returns false
-function checkExists(searchID, array) {
+export function checkExists(searchID, array) {
     let i = 0;
     for (const element of array) {
          // the ID of a channel or user is always the first value in both objects
