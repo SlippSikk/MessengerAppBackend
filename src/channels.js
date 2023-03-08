956d254e9c3e7aa7b40c64afb6d3584fc955b20e
Christopher <z5373462@ad.unsw.edu.au>
2023-03-08 16:51:40 +1100

    updateed channelListAll files and tests with new Data.md structure

--- src/channels.js
+++ src/channels.js
@@ -70,15 +70,20 @@ function channelsCreateV1(authUserId, name, isPublic) {
     };
 }
 
+/**
+ * @param {int} authUserId 
+ * @returns { channels: [{ channelId: integer, channelName: string}] }
+ * 
+ */
 function channelsListAllV1(authUserId) {
     let dataStore = getData();
     if (isValid(authUserId)) return { error: 'authUserId not valid' };
     const channelsObject = { channels: [] };
     for (let a of dataStore.channels) {
-        if (a.allMembers.includes(authUserId)) {
+        if (a.memberIds.includes(authUserId)) {
             channelsObject.push({
                 channelId: a.channelId,
-                name: a.name,
+                channelName: a.channelName,
             });
         }
     }
@@ -86,15 +91,15 @@ function channelsListAllV1(authUserId) {
 
 }
 /**
- *  
+ *  o
  * @param {integer} authUserId 
- * @returns {boolean} 
+ * @returns {bolean} 
  * note: check  if authUserId is valid/notValid
  */
 function isValid(authUserId) {
     let dataStore = getData();
     for (let a of dataStore.users) {
-        if (a.id === authUserId) return true;
+        if (a.userID === authUserId) return true;
     }
     return false;
 }
