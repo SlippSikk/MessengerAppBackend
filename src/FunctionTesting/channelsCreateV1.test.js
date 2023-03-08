676bf951ebf1cbb7bf551e1d662fce21d9ae4e05
Albert Kim <z5358832@ad.unsw.edu.au>
2023-03-08 14:56:37 +1100

    channelsCreateV1.test: Modified a few tests to work better

--- src/FunctionTesting/channelsCreateV1.test.js
+++ src/FunctionTesting/channelsCreateV1.test.js
@@ -4,8 +4,6 @@ import { authRegisterV1 } from './../auth.js';
 
 beforeEach(() => {
     clearV1();
-    let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
-    let invalidUserId = authUserId + 1;
 });
 
 
@@ -13,37 +11,54 @@ beforeEach(() => {
 describe('channelsCreateV1 Success Test', () => {
 
     test('Test 1', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
+
         expect(channelsCreateV1(authUserId, 'chat', true)).toEqual({ channelId: expect.any(Number) });
     });
 
     test('Test 2', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         expect(channelsCreateV1(authUserId, 'chat123CHAT', true)).toEqual({ channelId: expect.any(Number) });
     });
 
     test('Test 3', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         expect(channelsCreateV1(authUserId, 'chat', false)).toEqual({ channelId: expect.any(Number) });
     });
 
     test('Test 4', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         expect(channelsCreateV1(authUserId, 'chat123CHAT', false)).toEqual({ channelId: expect.any(Number) });
     });
 
     test('Test 5', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         let authUserId_2 = authRegisterV1("adam.baqaie@gmail.com", "adam123", "Adam", "Baqaie");
         expect(channelsCreateV1(authUserId_2, 'chat', true)).toEqual({ channelId: expect.any(Number) });
     });
 
     test('Test 6', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         let authUserId_2 = authRegisterV1("adam.baqaie@gmail.com", "adam123", "Adam", "Baqaie");
         expect(channelsCreateV1(authUserId_2, 'chat123CHAT', true)).toEqual({ channelId: expect.any(Number) });
     });
 
     test('Test 7', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         let authUserId_2 = authRegisterV1("adam.baqaie@gmail.com", "adam123", "Adam", "Baqaie");
         expect(channelsCreateV1(authUserId_2, 'chat', false)).toEqual({ channelId: expect.any(Number) });
     });
 
     test('Test 8', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         let authUserId_2 = authRegisterV1("adam.baqaie@gmail.com", "adam123", "Adam", "Baqaie");
         expect(channelsCreateV1(authUserId_2, 'chat123CHAT', false)).toEqual({ channelId: expect.any(Number) });
     });
@@ -54,35 +69,51 @@ describe('channelsCreateV1 Success Test', () => {
 describe('channelsCreateV1 Error Test', () => {
 
     test('Test 1: Invalid name', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         expect(channelsCreateV1(authUserId, '', true)).toEqual({ error: expect.any(String) });
     });
 
     test('Test 2: Invalid name', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         expect(channelsCreateV1(authUserId, '', false)).toEqual({ error: expect.any(String) });
     });
 
     test('Test 3: Invalid name', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         expect(channelsCreateV1(authUserId, 'asdflaskdjfhakjhflkasjhdl', true)).toEqual({ error: expect.any(String) });
     });
 
     test('Test 4: Invalid name', () => {
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
         expect(channelsCreateV1(authUserId, 'asdflaskdjfhakjhflkasjhdl', false)).toEqual({ error: expect.any(String) });
     });
 
     test('Test 5: Invalid userId', () => {
-        expect(channelsCreateV1(invalidUserId + 1, '', true)).toEqual({ error: expect.any(String) });
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
+        expect(channelsCreateV1(invalidUserId, '', true)).toEqual({ error: expect.any(String) });
     });
 
     test('Test 6: Invalid userId', () => {
-        expect(channelsCreateV1(invalidUserId + 1, '', false)).toEqual({ error: expect.any(String) });
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
+        expect(channelsCreateV1(invalidUserId, '', false)).toEqual({ error: expect.any(String) });
     });
 
     test('Test 7: Invalid userId', () => {
-        expect(channelsCreateV1(invalidUserId + 1, 'asdflaskdjfhakjhflkasjhdl', true)).toEqual({ error: expect.any(String) });
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
+        expect(channelsCreateV1(invalidUserId, 'asdflaskdjfhakjhflkasjhdl', true)).toEqual({ error: expect.any(String) });
     });
 
     test('Test 8: Invalid userId', () => {
-        expect(channelsCreateV1(invalidUserId + 1, 'asdflaskdjfhakjhflkasjhdl', false)).toEqual({ error: expect.any(String) });
+        let authUserId = authRegisterV1("albert.kim@gmail.com", "albert123", "Albert", "Kim");
+        let invalidUserId = authUserId + 1;
+        expect(channelsCreateV1(invalidUserId, 'asdflaskdjfhakjhflkasjhdl', false)).toEqual({ error: expect.any(String) });
     });
 
 });
