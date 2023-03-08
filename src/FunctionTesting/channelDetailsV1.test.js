1dbd1b698ec5f3af98a3dae07e17c8e8a2b0795d
Christopher <z5373462@ad.unsw.edu.au>
2023-03-08 23:06:20 +1100

    fixed minor bugs

--- src/FunctionTesting/channelDetailsV1.test.js
+++ src/FunctionTesting/channelDetailsV1.test.js
@@ -11,13 +11,13 @@ describe('test channelDetailsV1', () => {
           clearV1();
           let authUserId = authRegisterV1('duck@gmail.com', 1234, 'duck', 'dash');
           let authUserId2 = authRegisterV1('dog@gmail.com', 1234, 'dog', 'drown');
-          let courseId1Public = channelsCreateV1((authUserId, 'first', true));
-          let courseId2Public = channelsCreateV1((authUserId, 'second', true));
-          let courseId3NotPublic = channelsCreateV1((authUserId, 'third', false));
+          let courseId1Public = channelsCreateV1(authUserId, 'first', true);
+          let courseId2Public = channelsCreateV1(authUserId, 'second', true);
+          let courseId3NotPublic = channelsCreateV1(authUserId, 'third', false);
 
      });
      test('Test for Invalid authUserId', () => expect(channelDetailsV1(authUserId + 1, courseId1Public)).toBe(ERROR));
-     test('Test for Invalid courseId', () => expect(channelDetailsV1(authUserId, courseId1Public)).toBe(ERROR));
+     test('Test for Invalid courseId', () => expect(channelDetailsV1(authUserId, courseId1Public + 1)).toBe(ERROR));
      test('Test for not a member', () => expect(channelDetailsV1(authUserId2, courseId1Public)).toBe(ERROR));
 
      test('Test: call function -> public courseId ', () => {
