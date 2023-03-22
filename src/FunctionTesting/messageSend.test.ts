e86e77a352ec6bf94311a35c9278a8665f0597fa
Christopher <z5373462@ad.unsw.edu.au>
2023-03-22 14:16:35 +1100

    added more test cases for message send with requests included

--- src/FunctionTesting/messageSend.test.ts
+++ src/FunctionTesting/messageSend.test.ts
@@ -5,7 +5,7 @@ const SERVER_URL = `${url}:${port}`;
 const ERROR = { error: expect.any(String) };
 
 
-function requestPostCreate(token: string, channelId: number, message: string) {
+function requestMessageSend(token: string, channelId: number, message: string) {
   const res = request(
     'POST',
     SERVER_URL + '/message/send/v1',
@@ -20,19 +20,47 @@ function requestPostCreate(token: string, channelId: number, message: string) {
   return JSON.parse(res.getBody() as string);
 }
 
+let registered1: authUserId;
+let registered2: authUserId;
+let channelId1: number;
+let channelId1: number;
 
+beforeEach(() => {
+  requestClear();
+  registered1 = requestAuthRegister('duck@gmail.com', 'duck123', 'duck', 'dash');
+  registered2 = requestAuthRegister('chick@gmail.com', 'chick123', 'chick', 'mafia');
+  channelId1 = requestChannelsCreate(registered1.authUserId, 'nest', true).channelId;
+  channelId2 = requestChannelsCreate(registered2.authUserId, 'shed', true).channelId;
+});
 describe('Error Cases', () => {
+  test('Invalid channelId', () => {
+    expect(requestMessageSend(registered1.token, channelId1 + 1, 'Hi my ducklings')).toStrictEqual(ERROR);
+  });
+  test('Message must be between 1 to 1000 letters', () => {
+    expect(requestMessageSend(registered1.token, channelId1, '')).toStrictEqual(ERROR);
+  });
+  test('Message must be between 1 to 1000 letters', () => {
+    let fullChar = '';
+    for (let i = 0; 1001 > i; i++) {
+      fullChar += 'p';
+    }
+    expect(requestMessageSend(registered1.token, channelId1, fullChar)).toStrictEqual(ERROR);
+  });
+  test('User is not member of channel', () => {
+    expect(requestMessageSend(registered1.token, channelId2, 'Lets dance')).toStrictEqual(ERROR);
+  });
+  test('Invalid token', () => {
+    // white box testing
+    expect(requestMessageSend(registered1.token + 'p', channelId1, 'Lets dance')).toStrictEqual(ERROR);
+  });
+});
+
+describe('Function Testing', () => {
   beforeEach(() => {
-    
+
   });
-  test('Invalid postId', () => {
-    expect(requestPostView(currPostId + 1)).toStrictEqual(ERROR);
-  });
-  test('Post with no comments', () => {
-   
+  test('Invalid token', () => {
+
   });
 
-  test('Full Posts and more than 1 comment', () => {
-    
-    });
 });
