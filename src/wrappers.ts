9a03d837cab68291398132ad37cf43c7a9aa4347
Adam Baqaie <z5362016@ad.unsw.edu.au>
2023-03-26 17:08:54 +1100

    added function implementation, testing, http request and func wrapper as well as helper fucntions

--- src/wrappers.ts
+++ src/wrappers.ts
@@ -1,5 +1,6 @@
 import request from 'sync-request';
 import config from './config.json';
+import { dmId } from './interfaces'
 
 const port = config.port;
 const url = config.url;
@@ -7,90 +8,105 @@ const url = config.url;
 const SERVER_URL = `${url}:${port}`;
 
 function requestAuthRegister(email: string, password: string, nameFirst: string, nameLast: string) {
-  const res = request(
-    'POST',
-    `${url}:${port}/auth/register/v2`,
-    {
-      json: {
-        email,
-        password,
-        nameFirst,
-        nameLast
-      }
-    }
-  );
+    const res = request(
+        'POST',
+        `${url}:${port}/auth/register/v2`,
+        {
+            json: {
+                email,
+                password,
+                nameFirst,
+                nameLast
+            }
+        }
+    );
 
-  return JSON.parse(res.getBody() as string);
+    return JSON.parse(res.getBody() as string);
 }
 
 function requestAuthLogin(email: string, password: string) {
-  const res = request(
-    'POST',
-    `${url}:${port}/auth/login/v2`,
-    {
-      json: {
-        email,
-        password
-      }
-    }
-  );
+    const res = request(
+        'POST',
+        `${url}:${port}/auth/login/v2`,
+        {
+            json: {
+                email,
+                password
+            }
+        }
+    );
 
-  return JSON.parse(res.getBody() as string);
+    return JSON.parse(res.getBody() as string);
 }
 
 function requestClear() {
-  const res = request(
-    'DELETE',
-    `${url}:${port}/clear/v1`,
-    {
+    const res = request(
+        'DELETE',
+        `${url}:${port}/clear/v1`,
+        {
 
-    }
-  );
+        }
+    );
 
-  return JSON.parse(res.getBody() as string);
+    return JSON.parse(res.getBody() as string);
 }
 
 export function requestAddowner(token: string, channelId: number, uId: number) {
-  const res = request(
-    'POST',
-    SERVER_URL + '/channel/addowner/v1',
-    {
-      json: {
-        token: token,
-        channelId: channelId,
-        uId: uId
-      }
-    }
-  );
-  return JSON.parse(res.getBody() as string);
+    const res = request(
+        'POST',
+        SERVER_URL + '/channel/addowner/v1',
+        {
+            json: {
+                token: token,
+                channelId: channelId,
+                uId: uId
+            }
+        }
+    );
+    return JSON.parse(res.getBody() as string);
 }
 
 export function requestAuthLogout(token: string) {
-  const res = request(
-    'POST',
-    `${url}:${port}/auth/logout/v1`,
-    {
-      json: {
-        token
-      }
-    }
-  );
+    const res = request(
+        'POST',
+        `${url}:${port}/auth/logout/v1`,
+        {
+            json: {
+                token
+            }
+        }
+    );
 
-  return JSON.parse(res.getBody() as string);
+    return JSON.parse(res.getBody() as string);
 }
 
 export function requestChannelLeave(token: string, channelId: number) {
-  const res = request(
-    'POST',
-    SERVER_URL + '/channel/leave/v1',
-    {
-      json: {
-        token: token,
-        channelId: channelId,
-      }
-    }
-  );
-  return JSON.parse(res.getBody() as string);
+    const res = request(
+        'POST',
+        SERVER_URL + '/channel/leave/v1',
+        {
+            json: {
+                token: token,
+                channelId: channelId,
+            }
+        }
+    );
+    return JSON.parse(res.getBody() as string);
 }
+
+export function requestDmCreate(token: string, uIds: number[]): dmId {
+    const res = request(
+        'POST',
+        `${url}:${port}/dm/create/v1`,
+        {
+            json: {
+                token,
+                uIds
+            }
+        }
+    );
+
+    return JSON.parse(res.getBody() as string);
+};
 
 export { requestAuthRegister, requestAuthLogin, requestClear };
