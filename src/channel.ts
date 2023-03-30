e4f9dd308f4992bf6ee7179c0e140eb796164466
Christopher <z5373462@ad.unsw.edu.au>
2023-03-30 20:40:09 +1100

    moved some files to their correct location

--- src/channel.ts
+++ src/channel.ts
@@ -250,3 +250,27 @@ export const channelDetailsV2 = (token: string, channelId: number): channelDetai
   return returnObject;
 };
 */
+/*
+export const channelLeaveV1 = (token: string, channelId: number) => {
+  if (!isChannelIdValid(channelId)) {
+    return { error: 'Invalid channelId' };
+  }
+  if (!isTokenValid(token)) {
+    return { error: 'Invalid token' };
+  }
+  const uId = getUIdFromToken(token);
+  if (!isMember(channelId, uId)) {
+    return { error: 'user not a member of channelId' };
+  }
+  const channel = getChannel(channelId);
+  // remove ownership
+  if (isOwner(channelId, uId)) {
+    const ownerIndex = channel.ownerMembers.indexOf(uId);
+    channel.ownerMembers.splice(ownerIndex, 1);
+  }
+  // remove membership
+  const memberIndex = channel.allMembers.indexOf(uId);
+  channel.allMembers.splice(memberIndex, 1);
+  return {};
+};
+*/
