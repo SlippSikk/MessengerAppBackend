c245175ae5f4c15e8b2ac8a5433c8cfb38d4a559
Xuji Li <z5423791@ad.unsw.edu.au>
2023-04-08 00:13:19 +1000

    transfer test to it3 ver

--- src/ServerTesting/setName.test.ts
+++ src/ServerTesting/setName.test.ts
@@ -1,36 +1,57 @@
-import { requestClear, requestAuthRegister, requestSetNameV1, requestUserProfileV2 } from '../wrappers';
+import { requestClear, requestAuthRegister, requestSetNameV2, requestUserProfileV3 } from '../wrappers';
 const ERROR = { error: expect.any(String) };
 
 beforeEach(() => {
   requestClear();
+  const tokenA = requestAuthRegister('csgo@gmail.com', 'counterStrike', 'Ab', 'CDE').token;
 });
 
 describe('/user/profile/setname/v1', () => {
-  describe('error', () => {
+  test('return value', () => {
     requestClear();
     const tokenA = requestAuthRegister('csgo@gmail.com', 'counterStrike', 'Ab', 'CDE').token;
-    test.each([
-      { token: tokenA, nameFirst: 'ILoveAnimationGirlAndTheyAreMyWaifuHeiHeiHeiHeiHeiHei', nameLast: 'IchiRu' },
-      { token: tokenA, nameFirst: 'ShiRaSe', nameLast: 'ILoveAnimationGirlAndTheyAreMyWaifuHeiHeiHeiHeiHeiHei' },
-      { token: tokenA + 'A', nameFirst: 'HuaiZhi', nameLast: 'Li' },
-      { token: tokenA, nameFirst: 'HuaiZhi', nameLast: '' },
-      { token: tokenA, nameFirst: '', nameLast: 'Li' },
-    ])('token=$token, nameFirst=$nameFirst, nameLast=$nameLast', ({ token, nameFirst, nameLast }) => {
-      expect(requestSetNameV1(token, nameFirst, nameLast)).toStrictEqual(ERROR);
+    expect(requestSetNameV2('DeathLoop', 'ABCD')).toStrictEqual({});// more tests needed when other function finished
+  });
+
+  describe('error', () => {
+    test('nameFirst is too long', () => {
+      requestClear();
+      const registerObject = requestAuthRegister('csgo3@gmail.com', 'counterStrike', 'de', 'bug');
+      expect(requestSetNameV2('ILoveAnimationGirlAndTheyAreMyWaifuHeiHeiHeiHeiHeiHei','Ichiru').statusCode).toBe(400);
+      expect(requestSetNameV2('ILoveAnimationGirlAndTheyAreMyWaifuHeiHeiHeiHeiHeiHei','Ichiru').body.error).toStrictEqual({ message: expect.any(String) })
+    });
+    test('nameLast is too long', () => {
+      requestClear();
+      const registerObject = requestAuthRegister('csgo3@gmail.com', 'counterStrike', 'de', 'bug');
+      expect(requestSetNameV2('Ichiru','ILoveAnimationGirlAndTheyAreMyWaifuHeiHeiHeiHeiHeiHei').statusCode).toBe(400);
+      expect(requestSetNameV2('Ichiru','ILoveAnimationGirlAndTheyAreMyWaifuHeiHeiHeiHeiHeiHei').body.error).toStrictEqual({ message: expect.any(String) })
+    });
+    test('nameFirst is empty', () => {
+      requestClear();
+      const registerObject = requestAuthRegister('csgo3@gmail.com', 'counterStrike', 'de', 'bug');
+      expect(requestSetNameV2('','Ichiru').statusCode).toBe(400);
+      expect(requestSetNameV2('','Ichiru').body.error).toStrictEqual({ message: expect.any(String) })
+    });
+    test('nameLast is empty', () => {
+      requestClear();
+      const registerObject = requestAuthRegister('csgo3@gmail.com', 'counterStrike', 'de', 'bug');
+      expect(requestSetNameV2('Ichiru','').statusCode).toBe(400);
+      expect(requestSetNameV2('Ichiru','').body.error).toStrictEqual({ message: expect.any(String) })
     });
   });
 
   test('return value', () => {
     requestClear();
-    const tokenA = requestAuthRegister('csgo@gmail.com', 'counterStrike', 'Ab', 'CDE').token;
-    expect(requestSetNameV1(tokenA, 'DeathLoop', 'ABCD')).toStrictEqual({});// more tests needed when other function finished
+    const tokenA = requestAuthRegister('csgo@gmail.com', 'counterStrike', 'Ab', 'CDE');
+    expect(requestSetNameV2('DeathLoop', 'ABCD').statusCode).toBe(200);
+    expect(requestSetNameV2('DeathLoop', 'ABCD').body).toStrictEqual({})
   });
-
   test('reset the name', () => {
     requestClear();
     const registerObject = requestAuthRegister('csgo@gmail.com', 'counterStrike', 'Ab', 'CDE');
-    requestSetNameV1(registerObject.token, 'Ichiru', 'Shirase');
-    expect(requestUserProfileV2(registerObject.token, registerObject.authUserId)).toStrictEqual({
+    requestSetNameV2('Ichiru', 'Shirase');
+    expect(requestSetNameV2('Ichiru', 'Shirase').statusCode).toBe(200);
+    expect(requestUserProfileV3(registerObject.authUserId).body).toStrictEqual({
       user: {
         uId: registerObject.authUserId,
         email: 'csgo@gmail.com',
