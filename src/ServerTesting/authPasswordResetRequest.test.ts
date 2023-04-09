2d17e036fac9a01ffcf9ba594c1582f57351ff19
Adam Baqaie <z5362016@ad.unsw.edu.au>
2023-04-09 12:47:21 +1000

    fixed linting

--- src/ServerTesting/authPasswordResetRequest.test.ts
+++ src/ServerTesting/authPasswordResetRequest.test.ts
@@ -1,19 +1,14 @@
 import { requestAuthRegister, requestClear } from '../wrappers';
-import { authUserId } from '../interfaces';
 import { requestAuthPasswordResetRequest } from '../adamWrappers';
 
-
-// Global
-let registered: authUserId;
 beforeEach(() => {
-    requestClear();
-    registered = requestAuthRegister('ilyas.baqaie@gmail.com', 'adam123', 'Adam', 'Baqaie');
+  requestClear();
+  requestAuthRegister('ilyas.baqaie@gmail.com', 'adam123', 'Adam', 'Baqaie');
 });
 
 describe('Email Sent', () => {
-    test('Correct Inputs', () => {
-        expect(requestAuthPasswordResetRequest('ilyas.baqaie@gmail.com').body).toStrictEqual({});
-        expect(requestAuthPasswordResetRequest('ilyas.baqaie@gmail.com').statusCode).toBe(200);
-    });
+  test('Correct Inputs', () => {
+    expect(requestAuthPasswordResetRequest('ilyas.baqaie@gmail.com').body).toStrictEqual({});
+    expect(requestAuthPasswordResetRequest('ilyas.baqaie@gmail.com').statusCode).toBe(200);
+  });
 });
-
