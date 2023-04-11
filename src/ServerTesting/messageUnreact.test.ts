ff2b6c97cdf37262f6ede3c413fb38a83e26e932
Christopher Ardiwinata <z5373462@ad.unsw.edu.au>
2023-04-11 20:30:32 +1000

    added funciton file and testfile

--- src/ServerTesting/messageUnreact.test.ts
+++ src/ServerTesting/messageUnreact.test.ts
@@ -1,40 +1,37 @@
-test('Invalid channelId', () => {
-  expect(1 + 1).toStrictEqual(2);
-});
-// const token = req.header('token');
-// const { other, data } = req.body;
-/*
-export function requestMessageUnreact(token: string, messageId: number, reactId: number) {
-  const res = request(
-    'POST',
-    SERVER_URL + 'message/unreact/v1',
-    {
-      json: {
-        token,
-        messageId,
-        number
-      }
-    }
-  );
+// export function requestMessageUnreact(token: string, messageId: number, reactId: number) {
+//   const res = request(
+//     'POST',
+//     SERVER_URL + 'message/unreact/v1',
+//     {
+//   headers: { token },
+//       json: {
+//
+//         messageId,
+//         number
+//       }
+//     }
+//   );
+// const body = JSON.parse(res.body as string);
+// const statusCode = res.statusCode;
 
-  return JSON.parse(res.getBody() as string);
-}
-app.post('message/unreact/v1', (req: Request, res: Response) => {
-  const token = req.header('token');
-  const { messageId, reactId } = req.body;
+// return { body, statusCode };
 
-  res.json(messageReactV1(token, +messageId, +reactId));
-});
+// }
+// app.post('message/unreact/v1', (req: Request, res: Response) => {
+//   const token = req.header('token');
+//   const { messageId, reactId } = req.body;
+
+//   res.json(messageUnreactV1(token, +messageId, +reactId));
+// });
 import { requestAuthRegister, requestClear, requestMessageSend, requestChannelsCreate } from '../wrappers';
-import { requestChannelMessages, requestMessageReact, requestMessagUnreact, requestDmMessages, requestMessageSenddm, requestDmCreate } from '../wrappers';
+import { requestChannelMessages, requestMessageReact, requestMessageUnreact, requestDmMessages, requestMessageSenddm, requestDmCreate } from '../wrappers';
 import { authUserId } from '../interfaces';
-const ERROR = { code: 400, error: expect.any(String) };
 
 let registered1: authUserId;
 let registered2: authUserId;
 let registered3: authUserId;
 let channelId1: number;
-let channelId2: number;
+// let channelId2: number;
 let mIdChannel: number;
 let mIdDm: number;
 let dmId: number;
@@ -44,21 +41,21 @@ beforeEach(() => {
   registered1 = requestAuthRegister('duck@gmail.com', 'duck123', 'duck', 'dash');
   registered2 = requestAuthRegister('chick@gmail.com', 'chick123', 'chick', 'mafia');
   registered3 = requestAuthRegister('hound@gmail.com', 'hound123', 'dog', 'drown');
-  channelId1 = requestChannelsCreate(registered1.token, 'nest', true).channelId;
-  channelId2 = requestChannelsCreate(registered2.token, 'shed', true).channelId;
-  dmId = requestDmCreate(registered2.token, [registered1.authUserId, registered2.authUserId]).dmId;
-  mIdChannel = requestMessageSend(registered1.token, channelId1, 'Hi my ducklings').messageId;
-  mIdDm = requestMessageSenddm(registered1.token, dmId, 'Hi my dogs').messageId;
+  channelId1 = requestChannelsCreate(registered1.token, 'nest', true).body.channelId;
+  // channelId2 = requestChannelsCreate(registered2.token, 'shed', true).body.channelId;
+  dmId = requestDmCreate(registered2.token, [registered1.authUserId]).dmId;
+  mIdChannel = requestMessageSend(registered1.token, channelId1, 'Hi my ducklings').body.messageId;
+  mIdDm = requestMessageSenddm(registered1.token, dmId, 'Hi my dogs').body.messageId;
   requestMessageReact(registered1.token, mIdChannel, 1);
   requestMessageReact(registered1.token, mIdDm, 1);
 });
 
 describe('Error Cases', () => {
   test('Invalid token', () => {
-    expect(requestMessagUnreact(registered1.token + registered2.token, mIdChannel, 1).statusCode).toStrictEqual(403);
+    expect(requestMessageUnreact(registered1.token + registered2.token, mIdChannel, 1).statusCode).toStrictEqual(403);
   });
   test('Invalid messageId', () => {
-    expect(requestMessagUnreact(registered1.token, mIdChannel * mIdDm + 1, 1).statusCode).toStrictEqual(400);
+    expect(requestMessageUnreact(registered1.token, mIdChannel * mIdDm + 1, 1).statusCode).toStrictEqual(400);
   });
   test('user is not in channel/dm of messageId', () => {
     expect(requestMessageUnreact(registered3.token, mIdChannel, 1).statusCode).toStrictEqual(400);
@@ -72,16 +69,15 @@ describe('Function Testing', () => {
   test('Double unreacts in channel msg', () => {
     expect(requestMessageUnreact(registered1.token, mIdChannel, 1).body).toStrictEqual({});
     expect(requestMessageUnreact(registered1.token, mIdChannel, 1).statusCode).toStrictEqual(400);
-    const a = requestChannelMessages(registered1.token, channelId1, 0);
-    expect(a.messages[0].message.reacts.reactId).toStrictEqual(1);
-    expect(a.messages[0].message.reacts.allUsers.length).toStrictEqual(0);
+    const a = requestChannelMessages(registered1.token, channelId1, 0).body;
+    expect(a.messages[0].reacts[0].reactId).toStrictEqual(1);
+    expect(a.messages[0].reacts[0].allUsers[0].uId).toStrictEqual(0);
   });
   test('Double unreacts in dm msg', () => {
     expect(requestMessageUnreact(registered1.token, mIdDm, 1).body).toStrictEqual({});
     expect(requestMessageUnreact(registered1.token, mIdDm, 1).statusCode).toStrictEqual(400);
-    const a = requestDmMessages(registered1.token, dmId, 0);
-    expect(a.messages[0].message.reacts.reactId).toStrictEqual(1);
-    expect(a.messages[0].message.reacts.allUsers.length).toStrictEqual(0);
+    const a = requestDmMessages(registered2.token, dmId, 0).body;
+    expect(a.messages[0].reacts[0].reactId).toStrictEqual(1);
+    expect(a.messages[0].reacts[0].allUsers[0].uId).toStrictEqual(0);
   });
 });
-*/
--- src/messageUnreact.ts
+++ src/messageUnreact.ts
@@ -1,38 +1,118 @@
-/*
-import { receiveMessageOnPort } from 'worker_threads';
 import { getData, setData } from './dataStore';
-import { isTokenValid, isMessageInChannel, findChannelIndexWithMessage, getUIdFromToken, isOwnerByToken, isMember, isMessageInDM, findDMIndexWithMessage, isDmMember, getUser } from './helper';
-import { isDmIdValid, createMessageId, validateToken, getMessage } from './helper';
-import { dataTs, channel, dms, messages, user } from './interfaces';
+import { getUIdFromToken, findChannelIndexWithMessage, findDMIndexWithMessage } from './helper';
+import { validateToken, isMember, isDmMember } from './helper';
+import { dataTs, messages, channel, dms } from './interfaces';
 import HTTPError from 'http-errors';
+
+const getChannelFromMessageId = (messageId: number): channel => {
+  const data: dataTs = getData();
+  return data.channels.find(channel => channel.messages.find(message => message.messageId === messageId));
+};
+
+const getDmFromMessageId = (messageId: number): dms => {
+  const data: dataTs = getData();
+  return data.dms.find(dm => dm.messages.find(message => message.messageId === messageId));
+};
+/**
+ * @param {number} messageId
+ * @returns {object} msg object
+ * @summary Gets message object
+ */
+export const getMessage = (messageId: number): messages | boolean => {
+  const data: dataTs = getData();
+  let msg;
+  for (const channel of data.channels) {
+    msg = channel.messages.find(message => message.messageId === messageId);
+    if (msg) {
+      return msg;
+    }
+  }
+  for (const dm of data.dms) {
+    msg = dm.messages.find(message => message.messageId === messageId);
+    if (msg) {
+      return msg;
+    }
+  }
+  return false;
+};
 /**
  * @param token
  * @param dmId
  * @param message
- * @returns
+ * @summary unReacts reaction of reactId, which is in a message from messageId
  */
-/*
+
 export const messageUnreactV1 = (token: string, messageId: number, reactId: number) => {
-  const data: dataTs = getData();
   if (!validateToken(token)) {
     throw HTTPError(403, 'Invalid token');
   }
-  const msg = getMessage(messageId) as messages;
-  if (!msg) {
+  const data: dataTs = getData();
+  let inChannel = true;
+  let inDm = true;
+  let dms;
+  const channel = getChannelFromMessageId(messageId);
+  if (!channel) {
+    dms = getDmFromMessageId(messageId);
+    inChannel = false;
+  }
+  if (!dms) {
+    inDm = false;
+  }
+  // --------- CHECKS IF IS MESSAGEID ISVALID -----------------------
+  // Checks if messageId is in channel or a dm
+  if ((!inChannel && !inDm) || (inChannel && inDm)) {
     throw HTTPError(400, 'Invalid messageId');
+  }
+  const uId = getUIdFromToken(token);
+  // Check if user is in Channel
+  let channelId: number;
+  if (inChannel) {
+    channelId = channel.channelId;
+    if (!isMember(channelId, uId) && uId !== 1) {
+      throw HTTPError(400, 'not a member of messageId');
+    }
+  }
+  // Check if user is in dm
+  let dmId: number;
+  if (inDm) {
+    dmId = dms.dmId;
+    if (!isDmMember(dmId, token)) {
+      throw HTTPError(400, 'not a member of messageId');
+    }
   }
   if (reactId !== 1) {
     throw HTTPError(400, 'Invalid ReactId');
   }
+  const msg = getMessage(messageId) as messages;
   const indexReactId = reactId - 1;
-  const uId = getUIdFromToken(token) as number;
   if (!msg.reacts[indexReactId].allUsers.find(user => user.uId === uId)) {
-    throw HTTPError(400, 'no Reaction');
+    throw HTTPError(400, 'no reactions');
   }
-  ////////////////////////////
-  const indexToRemove = msg.reacts[indexReactId].allUsers.findIndex(user => user.uId === getUIdFromToken(token) as number);
-  msg.reacts[indexReactId].allUsers.splice(indexToRemove, 1);
+  // const user = getUser(uId);
+  const indexToRemove = msg.reacts[indexReactId].allUsers.findIndex(user => user.uId === uId);
+  if (inChannel) {
+    let mIndex;
+    const channelIndex = findChannelIndexWithMessage(messageId);
+    const len = data.channels[channelIndex].messages.length;
+    for (let i = 0; i < len; i++) {
+      if (data.channels[channelIndex].messages[i].messageId === messageId) {
+        mIndex = i;
+        break;
+      }
+    }
+    data.channels[channelIndex].messages[mIndex].reacts[0].allUsers.splice(indexToRemove, 1);
+  } else {
+    let mIndex;
+    const dmIndex = findDMIndexWithMessage(messageId);
+    const len = data.dms[dmIndex].messages.length;
+    for (let i = 0; i < len; i++) {
+      if (data.dms[dmIndex].messages[i].messageId === messageId) {
+        mIndex = i;
+        break;
+      }
+    }
+    data.dms[dmIndex].messages[mIndex].reacts[0].allUsers.splice(indexToRemove, 1);
+  }
   setData(data);
   return {};
 };
-*/
